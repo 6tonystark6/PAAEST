9342a3d47a2d07c481ebd300ba73553226759032
Chihan Huang <139672193+6tonystark6@users.noreply.github.com>
2025-02-18 16:20:03 +0800

    Update model.py

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ class Model(nn.Module):
         out = self.decoder(t, mixed_feature_1, mixed_feature_2)
         return out
 
-    def forward(self, content_images, style_images, alpha=1.0, lam=10, beta=1.0, cycle_lambda=10.0):
+    def forward(self, content_images, style_images, alpha=1.0, lam=10, beta=1.0, cycle_lambda=10.0, id_lambda=5.0):
         content_features = self.vgg_encoder(content_images, output_last_feature=True)
         style_features = self.vgg_encoder(style_images, output_last_feature=True)
         decoder_input = self.generate_decoder_input(content_features, style_features)
@@ -83,8 +83,27 @@ class Model(nn.Module):
 
         loss_c = self.calc_content_loss(output_features, t)
         loss_s = self.calc_style_loss(output_middle_features, style_middle_features)
+        
         regenerated_content_features = self.vgg_encoder(out, output_last_feature=True)
-        loss_cycle = self.calc_cycle_loss(content_features, regenerated_content_features)
+        regenerated_decoder_input = self.generate_decoder_input(regenerated_content_features, content_features)
+        regenerated_t = alpha * regenerated_decoder_input + (1 - alpha) * regenerated_content_features
+        _, regenerated_content_temp_1, regenerated_content_temp_2, _ = self.vgg_encoder(out, output_last_feature=False)
+        regenerated_mixed_feature_1 = whiten_and_color(regenerated_content_temp_1, content_temp_1)
+        regenerated_mixed_feature_2 = whiten_and_color(regenerated_content_temp_2, content_temp_2)
+        regenerated_out = self.decoder(regenerated_t, regenerated_mixed_feature_1, regenerated_mixed_feature_2)
+        loss_cycle = F.mse_loss(content_images, regenerated_out)
 
-        loss = loss_c + lam * loss_s + cycle_lambda * loss_cycle
+        content_decoder_input = self.generate_decoder_input(content_features, content_features)
+        content_t = alpha * content_decoder_input + (1 - alpha) * content_features
+        content_mixed_feature_1 = whiten_and_color(content_temp_1, content_temp_1)
+        content_mixed_feature_2 = whiten_and_color(content_temp_2, content_temp_2)
+        content_out = self.decoder(content_t, content_mixed_feature_1, content_mixed_feature_2)
+        style_decoder_input = self.generate_decoder_input(style_features, style_features)
+        style_t = alpha * style_decoder_input + (1 - alpha) * style_features
+        style_mixed_feature_1 = whiten_and_color(style_temp_1, style_temp_1)
+        style_mixed_feature_2 = whiten_and_color(style_temp_2, style_temp_2)
+        style_out = self.decoder(style_t, style_mixed_feature_1, style_mixed_feature_2)
+        loss_id = F.mse_loss(content_images, content_out) + F.mse_loss(style_images, style_out)
+
+        loss = loss_c + lam * loss_s + cycle_lambda * loss_cycle + id_lambda * loss_id
         return loss
