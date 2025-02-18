c0347391fd30fb80c6feede0a7bbc3f5ff6757a2
Chihan Huang <139672193+6tonystark6@users.noreply.github.com>
2025-02-18 16:23:06 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,31 @@
 import torch_fidelity
-import os
 import numpy as np
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 from PIL import Image
+import os
+
+input_dir = './test_content_resized'
+output_dir = './test_content_resized1'
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for filename in os.listdir(input_dir):
+    input_file_path = os.path.join(input_dir, filename)
+
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+        with Image.open(input_file_path) as img:
+            img_resized = img.resize((512, 512))
+
+            output_file_path = os.path.join(output_dir, filename)
+
+            img_resized.save(output_file_path)
+            print(f"image {filename} resized and saved to {output_file_path}")
 
 folder1 = './output/transfer1'
 folder2 = './output/transfer2'
-folder3 = './test_content_resized'
+folder3 = './test_content_resized1'
 
 metrics = torch_fidelity.calculate_metrics(
     input1=folder1,
@@ -24,30 +42,24 @@ ssim_values = []
 
 for i in range(1, 1001):
     filename = f"{i}.jpg"
-    path2 = os.path.join(folder2, filename)
-    path3 = os.path.join(folder3, filename)
 
-    if not os.path.exists(path2) or not os.path.exists(path3):
+    path1 = os.path.join(folder2, filename)
+    path2 = os.path.join(folder3, filename)
+
+    if not os.path.exists(path1) or not os.path.exists(path2):
         print(f"file {filename} doesn't exist, skip...")
         continue
 
+    img1 = np.array(Image.open(path1).convert("RGB"))
     img2 = np.array(Image.open(path2).convert("RGB"))
-    img3 = Image.open(path3).convert("RGB")
 
-    width, height = img3.size
-    left = (width - 256) // 2
-    top = (height - 256) // 2
-    right = left + 256
-    bottom = top + 256
-    img3_cropped = img3.crop((left, top, right, bottom))
-    img3_cropped = np.array(img3_cropped)
+    if img1.shape != img2.shape:
+        # Resize img2 to match the shape of img1
+        img2_resized = Image.fromarray(img2).resize(img1.shape[1::-1])  # img1.shape[1::-1] gets the (width, height)
+        img2 = np.array(img2_resized)
 
-    if img2.shape != img3_cropped.shape:
-        print(f"file {filename} size doesn't match, skip...")
-        continue
-
-    psnr_value = psnr(img2, img3_cropped, data_range=img2.max() - img2.min())
-    ssim_value = ssim(img2, img3_cropped, data_range=img2.max() - img2.min(), multichannel=True)
+    psnr_value = psnr(img1, img2, data_range=img1.max() - img1.min())
+    ssim_value = ssim(img1, img2, data_range=img1.max() - img1.min(), multichannel=True)
 
     psnr_values.append(psnr_value)
     ssim_values.append(ssim_value)
